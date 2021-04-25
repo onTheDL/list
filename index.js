9e7b21a9fe3da0e071ab3075607748901c78ca01
Daniel Laserna <daniel.laserna@protonmail.com>
2021-04-25 18:05:04 -0400

    Add callback-based solution

--- index.js
+++ index.js
@@ -7,15 +7,51 @@ fs.readdir(process.cwd(), (err, filenames) => {
     console.log(err);
   }
 
-  // BAD CODE ...
+  /*
+ 
   for (let filename of filenames) {
     fs.lstat(filename, (err, stat) => {
       if(err) {
         console.log(err);
       }
-      console.log(filename, stat.isFile())
+
+     
     });
   }
-  // BAD CODE COMPLETE
+  
 
+  */
+
+  // creates an array with length of filenames, each value being null
+
+  // BAD CODE (see Lec 327)
+  const allStats = Array(filenames.length).fill(null);
+
+  for (let filename of filenames) {
+    const index = filenames.indexOf(filename);
+    fs.lstat(filename, (err, stats) => {
+      if (err) {
+        console.log(err);
+      }
+
+      // problem: console.log is run before all callbacks are resolved
+      // affects order of files logged
+
+      //SOLUTION 1
+      allStats[index] = stats;
+
+      // See if there is any null values left
+      const ready = allStats.every((stats) => stats);
+
+      // need index to reference stats back to filename
+      if (ready) {
+        allStats.forEach((stats, index) => {
+          console.log(filenames[index], stats.isFile());
+        });
+      }
+
+      // BAD CODE COMPLETE
+      // OPTIONAL SOLUTIONS (see Lec 328)
+    });
+  }
 });
