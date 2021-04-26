f7316632c15efa6c6249f6ebc8d5827e49a58091
Daniel Laserna <daniel.laserna@protonmail.com>
2021-04-26 15:29:31 -0400

    Implement Promise.all-based solution

--- index.js
+++ index.js
@@ -1,30 +1,16 @@
 #!/usr/bin/env node
 
+const { rejects } = require("assert");
 const fs = require("fs");
+const { resolve } = require("path");
+const util = require("util");
 
-fs.readdir(process.cwd(), (err, filenames) => {
-  if (err) {
-    console.log(err);
-  }
-
-  /*
- 
-  for (let filename of filenames) {
-    fs.lstat(filename, (err, stat) => {
-      if(err) {
-        console.log(err);
-      }
-
-     
-    });
-  }
-  
-
-  */
+/*
 
   // creates an array with length of filenames, each value being null
 
-  // BAD CODE (see Lec 327)
+  // BAD CODE (see Lec 327):  Maintain an array of the results from each lstat. As each cb is invoked, add stats object to this array.  When array is full, log everything in it
+
   const allStats = Array(filenames.length).fill(null);
 
   for (let filename of filenames) {
@@ -36,8 +22,9 @@ fs.readdir(process.cwd(), (err, filenames) => {
 
       // problem: console.log is run before all callbacks are resolved
       // affects order of files logged
-
-      //SOLUTION 1
+    
+    
+      // SOLUTION 1
       allStats[index] = stats;
 
       // See if there is any null values left
@@ -51,7 +38,75 @@ fs.readdir(process.cwd(), (err, filenames) => {
       }
 
       // BAD CODE COMPLETE
+      // - not ideal because harder to maintain as code becomes more complex
       // OPTIONAL SOLUTIONS (see Lec 328)
+
+    }); 
+  }
+  */
+
+// SOLUTION 2 (BETTER CODE):  Wrap lstat call with a promise, use async/await to process lstat call one at a time
+
+/*
+  // Method #1
+  const lstat = (filename) => {
+    return new Promise((res, rej) => {
+      fs.lstat(filename, (err, stats) => {
+        if (err) rejects(err);
+
+        resolve(stats)
+      });
     });
+  };
+  */
+
+/*
+  // Method #2
+  const lstat = util.promisify(fs.lstat)
+  */
+
+/*
+// Method #3
+const { lstat } = fs.promises;
+
+
+fs.readdir(process.cwd(), async (err, filenames) => {
+  if (err) {
+    console.log(err);
+  }
+
+  for (let filename of filenames) {
+    try {
+      const stats = await lstat(filename);
+      console.log(filename, stats.isFile());
+    } catch (err) {
+      console.error(err);
+    }
   }
 });
+*/
+
+// SOLUTION 3 (BEST):  Wrap the lstat call with a promise, use async/await + the Promise.all helper method to process lstat calls all at once
+
+const { lstat } = fs.promises;
+
+fs.readdir(process.cwd(), async (err, filenames) => {
+  if (err) {
+    console.log(err);
+  }
+
+  const statPromises = filenames.map(filename => { 
+    return lstat(filename)
+  })
+
+  const allStats = await Promise.all(statPromises)
+
+  for (let stats of allStats) {
+    const idx = allStats.indexOf(stats)
+    console.log(filenames[idx], stats.isFile())
+  }
+
+});
+
+
+
